0543754ca8d52ce6c8ba66576c095079da657700
saubhagya02-bit <saubhagyaushani03@gmail.com>
2026-08-30 13:53:42 +0530

    feat(admin): integrate Asgardeo AuthProvider

--- admin-portal/src/main.jsx
+++ admin-portal/src/main.jsx
@@ -1,11 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ViewStalls from './pages/ViewStalls.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider as AsgardeoProvider } from "@asgardeo/auth-react";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import App from "./App.jsx";
+import { authConfig } from "./authConfig.js";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AsgardeoProvider config={authConfig}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </AsgardeoProvider>
+  </React.StrictMode>,
+);
